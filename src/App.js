d464697b025ec472376971be3b8c345e288e9e24
DoHi0512 <donghuni512@gmail.com>
2022-07-05 19:54:59 +0900

    물건 리스트

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import React from "react";
 import Product_list from "./product_list";
-import Header from "./Header";
-import Home from "./Home";
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Product_list/>
     </div>
   );
 }
--- src/product_list.js
+++ src/product_list.js
@@ -1,61 +1,120 @@
-import React, { useState } from "react";
+import React from "react";
 import "./product_list.css";
 function Product_list() {
-    const [popup,setPopup] = useState(true);
-  function test() {
-    alert("hihihi");
+  function buy(index) {
+    if (index === -1) return;
+    alert(index);
   }
+  let tempData = {
+    idx: -1,
+    imgsrc: "https://www.analogouscolors.com/jpg/ffffff.jpg",
+    name: "",
+    price: "",
+  };
+  let products = [
+    {
+      imgsrc:
+        "https://sitem.ssgcdn.com/00/00/76/item/1000010760000_i1_1200.jpg",
+      idx: 1,
+      name: "스윙칩",
+      price: 1500,
+    },
+    {
+      imgsrc:
+        "https://sitem.ssgcdn.com/32/46/10/item/0000008104632_i1_1200.jpg",
+      idx: 2,
+      name: "포카칩",
+      price: 2000,
+    },
+    {
+      imgsrc:
+        "https://sitem.ssgcdn.com/97/72/72/item/1000019727297_i1_1200.jpg",
+      idx: 3,
+      name: "육즙만두",
+      price: 1000,
+    },
+    {
+      imgsrc:
+        "https://sitem.ssgcdn.com/32/46/10/item/0000008104632_i1_1200.jpg",
+      idx: 2,
+      name: "포카칩",
+      price: 2000,
+    },
+    {
+      imgsrc:
+        "https://sitem.ssgcdn.com/00/00/76/item/1000010760000_i1_1200.jpg",
+      idx: 1,
+      name: "스윙칩",
+      price: 1500,
+    },
+    {
+      imgsrc:
+        "https://sitem.ssgcdn.com/97/72/72/item/1000019727297_i1_1200.jpg",
+      idx: 3,
+      name: "육즙만두",
+      price: 1000,
+    },
+    {
+      imgsrc:
+        "https://sitem.ssgcdn.com/32/46/10/item/0000008104632_i1_1200.jpg",
+      idx: 2,
+      name: "포카칩",
+      price: 2000,
+    },
+    {
+      imgsrc:
+        "https://sitem.ssgcdn.com/00/00/76/item/1000010760000_i1_1200.jpg",
+      idx: 1,
+      name: "스윙칩",
+      price: 1500,
+    },
+    {
+      imgsrc:
+        "https://sitem.ssgcdn.com/97/72/72/item/1000019727297_i1_1200.jpg",
+      idx: 3,
+      name: "육즙만두",
+      price: 1000,
+    },
+    {
+      imgsrc:
+        "https://sitem.ssgcdn.com/32/46/10/item/0000008104632_i1_1200.jpg",
+      idx: 2,
+      name: "포카칩",
+      price: 2000,
+    },
+    {
+      imgsrc:
+        "https://sitem.ssgcdn.com/00/00/76/item/1000010760000_i1_1200.jpg",
+      idx: 1,
+      name: "스윙칩",
+      price: 1500,
+    },
+  ];
+  for (let i = 0; i < products.length % 5; i++) products.push(tempData);
+  const list = products.map((data) => (
+    <div className="prod">
+      <img
+        onClick={() => buy(data.idx)}
+        src={data.imgsrc}
+        width="150"
+        height="150"
+        alt="이미지가 표시되지 않음"
+      ></img>
+
+      <p>{data.name !== "" ? "이름 : " + data.name : ""}</p>
+      <p>{data.price !== "" ? "가격 : " + data.price : ""}</p>
+    </div>
+  ));
   return (
     <div className="product_list">
-      <header></header>
-      <section>
-        <table>
-          <tr>
-            <td onClick={test}>
-              <img src="C:\react-highlow\public\다운로드.jpg" alt = "no"/>
-            </td>
-            <td>
-              <img src="http://placehold.it/150x150" />
-            </td>
-            <td>
-              <img src="http://placehold.it/150x150" />
-            </td>
-            <td>
-              <img src="http://placehold.it/150x150" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img src="http://placehold.it/150x150" />
-            </td>
-            <td>
-              <img src="http://placehold.it/150x150" />
-            </td>
-            <td>
-              <img src="http://placehold.it/150x150" />
-            </td>
-            <td>
-              <img src="http://placehold.it/150x150" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img src="http://placehold.it/150x150" />
-            </td>
-            <td>
-              <img src="http://placehold.it/150x150" />
-            </td>
-            <td>
-              <img src="http://placehold.it/150x150" />
-            </td>
-            <td>
-              <img src="http://placehold.it/150x150" />
-            </td>
-          </tr>
-        </table>
-       
-      </section>
-      <footer></footer>
+      <div className="fixedSize">
+        <header>
+          <div className="mark">BSSM</div>
+        </header>
+        <div className="lists">{list}</div>
+
+        <footer>오태식</footer>
+      </div>
     </div>
   );
 }
