dd1c51c1a9eaa9fac7c797593ad47ae5f280857c
soohyang136 <jsh515228@gmail.com>
2022-10-17 13:56:31 +0900

    UPDATE::footer 배경 색 변경

--- src/components/Footer/footer.style.jsx
+++ src/components/Footer/footer.style.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Footer = styled.div`
     height: 45vh;
-    background-color: #F5F5F5;
+    background-color: #EBEBEB;
     width: 100%;
     margin-top: 10vh;
     display: flex;
