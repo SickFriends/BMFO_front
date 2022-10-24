3a53d7e685509d6876d5fd62d20970c4019e4cb6
soohyang136 <jsh515228@gmail.com>
2022-10-25 08:30:13 +0900

    UPDATE::구매 페이지 개수 추가 기능

--- src/pages/Purchase/index.jsx
+++ src/pages/Purchase/index.jsx
@@ -14,7 +14,18 @@ const Purchase = () => {
                 <P.titleBox>스윙칩</P.titleBox>
                 <P.priceBox>₩1,400</P.priceBox>
                 <P.count>수량</P.count>
-                <P.IncDec><AiOutlineMinusCircle size={30}/><div>{count}</div><AiOutlinePlusCircle size={30} /></P.IncDec>
+                <P.IncDec>
+                    <AiOutlineMinusCircle size={30} onClick={() => {
+                        console.log("d")
+                        if(count > 0){
+                            setCount(count-1);
+                        }
+                    }}/>
+                    <div>{count}</div>
+                    <AiOutlinePlusCircle size={30} onClick={() => {
+                        console.log("d");
+                        setCount(count+1);
+                    }} /></P.IncDec>
                 <P.buttons><button>장바구니</button><button>구매하기</button></P.buttons>
             </P.saleBox>
         </P.container>
