8aef823d7689113ecee7ac7a318753b83a653139
soohyang136 <jsh515228@gmail.com>
2022-11-07 00:59:07 +0900

    UPDATE::장바구니 페이지 구매 칸 구성 완료

--- src/pages/ShoppingBasket/index.jsx
+++ src/pages/ShoppingBasket/index.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import BasketBox from "../../components/BasketBox";
 import * as S from "./ShoppingBasket.style"
+import{AiOutlineArrowDown} from "react-icons/ai"
 
 const ShoppingBasket = () => {
+    const [sum, setSum] = useState(0);
     const productList = [
         {
             name: "새우깡",
@@ -57,15 +60,32 @@ const ShoppingBasket = () => {
             num: 2
         }
     ]
+    let allPrice  = 0;
+    for(let i = 0; i < productList.length; i++){
+        allPrice +=productList[i].price
+    }
     return (
         <S.Container>
             <S.Title>장바구니</S.Title>
             <S.MainBox>
                 <S.ProductList>
-                    {productList.map((data, index) => <BasketBox data={data} index={index} /> )}
+                    {productList.map((data, index) => {
+                            return <BasketBox data={data} index={index} /> 
+                        }
+                    
+                    )}
                 </S.ProductList>
                 <S.ButtonBox>
-
+                    <S.PayTitle>결제 예정 금액</S.PayTitle>
+                    <S.Cost>상품금액 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;₩{allPrice}원</S.Cost>
+                    <S.Cost>할인금액 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;₩0원</S.Cost>
+                    <S.Sum>
+                        <div>합계</div>
+                        <div>₩{allPrice}원</div>
+                    </S.Sum>
+                    <S.SetPasswordButton>비밀번호 설정</S.SetPasswordButton>
+                    <AiOutlineArrowDown size={50}/>
+                    <S.PurchaseButton>구매하기</S.PurchaseButton>
                 </S.ButtonBox>
             </S.MainBox>
         </S.Container>
