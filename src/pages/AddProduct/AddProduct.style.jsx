18ed6f4acce66d1068468b22e65150392cb92f1e
soohyang136 <jsh515228@gmail.com>
2022-11-24 01:42:43 +0900

    FEAT :: 제품 추가 기능

--- src/pages/AddProduct/AddProduct.style.jsx
+++ src/pages/AddProduct/AddProduct.style.jsx
@@ -29,6 +29,10 @@ export const ImgBox = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    img{
+        width: 100%;
+        height: 100%;
+    }
 `
 
 export const InfoBox = styled.div`
