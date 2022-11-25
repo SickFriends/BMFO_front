e4daed30a34589deecbb579794b200e13cda660f
DoHi0512 <donghuni512@gmail.com>
2022-11-25 09:14:33 +0900

    cocnnect

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import * as S from "./login.style";
 export default function Login() {
+  const [id, setId] = useState("");
+  const [pwd, setPwd] = useState("");
+  function sub() {
+    const form = { username: id, password: pwd };
+    console.log(form);
+    axios
+      .post("http://localhost:8080/user/login", form)
+      .then((res) => {
+        localStorage.setItem("token", res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
   return (
     <S.Wrapper>
       <h1>BMFO ID</h1>
       <S.InputContainer>
-        <input type="text" placeholder="이메일/아이디"></input>
-        <input type="password" placeholder="암호"></input>
+        <input
+          type="text"
+          placeholder="이메일/아이디"
+          onChange={(e) => setId(e.target.value)}
+        ></input>
+        <input
+          type="password"
+          placeholder="암호"
+          onChange={(e) => setPwd(e.target.value)}
+        ></input>
       </S.InputContainer>
-      <button>로그인</button>
-      <Link to="/signup" >
+      <button onClick={sub}>로그인</button>
+      <Link to="/signup">
         <p>BMFO ID 또는 암호를 잊으셨습니까?</p>
       </Link>
     </S.Wrapper>
--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,27 +1,55 @@
+import axios from "axios";
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import * as S from "./signup.style";
 export default function Signup() {
+  const [id, setId] = useState("");
+  const [pwd, setPwd] = useState("");
+  function sub() {
+    const form = { username: id, password: pwd };
+    console.log(form);
+    axios
+      .post("http://localhost:8080/user/signup", form)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
   return (
     <S.Wrapper>
       <h1>BMFO에 계정 생성</h1>
       <S.InputContainer>
         <span>새 BMFO에 사용될 이메일/아이디 입니다.</span>
-        <input type="text" placeholder="이메일/아이디"></input>
-        <input type="password" placeholder="암호"></input>
+        <input
+          type="text"
+          placeholder="이메일/아이디"
+          onChange={(e) => {
+            setId(e.target.value);
+          }}
+        ></input>
+        <input
+          type="password"
+          placeholder="암호"
+          onChange={(e) => {
+            setPwd(e.target.value);
+          }}
+        ></input>
         <input type="password" placeholder="암호 확인"></input>
       </S.InputContainer>
-      <Link to="/login" >
+      <Link to="/login">
         <p>이미 계정이 있으십니까?</p>
       </Link>
       <S.TextContainer>
         <input type="checkbox" />
-          <span>
-            BMFO의 개인정보 처리방침새 윈도우에서 열림에 따라 개인 정보를 수집,
-            사용, 타사에 대한 제공 및 처리하는 데 동의합니다.
-          </span>
+        <span>
+          BMFO의 개인정보 처리방침새 윈도우에서 열림에 따라 개인 정보를 수집,
+          사용, 타사에 대한 제공 및 처리하는 데 동의합니다.
+        </span>
       </S.TextContainer>
-      <button>계정 만들기</button>
+      <button onClick={sub}>계정 만들기</button>
     </S.Wrapper>
   );
 }
